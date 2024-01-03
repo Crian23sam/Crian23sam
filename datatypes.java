91c994935a5be04c0903c479cde9413c2d2340b4
Crian23sam <152302320+Crian23sam@users.noreply.github.com>
2024-01-03 19:31:55 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,33 @@
+class DataTypes
+{
+    public static void main(String[] args)
+    {
+        byte var1=21;
+        //char var2="A";
+        char var2 = 'A';
+        int var3=1234;
+        long var4=34928L;
+        double var5=25.43;
+        short var6=20;
+        //float var7=29.05;
+        float var7=29.05f;
+        boolean var8=true;
+
+        System.out.println("byte v1="+ var1);
+        
+        System.out.println("char v2="+ var2);
+        
+        System.out.println("int v3="+ var3);
+        
+        System.out.println("long v4="+ var4);
+        
+        System.out.println("double v5="+ var5);
+        
+        System.out.println("short v6="+ var6);
+        
+        System.out.println("float v7="+ var7);
+        
+        System.out.println("boolean v8="+ var8);
+        
+    }
+}
---
+++ operators.java
@@ -0,0 +1,23 @@
+public class operators {
+    public static void main(String[] args) {
+        byte var1=21;
+        short var2=25;
+        System.out.println(var1 + var2);
+        System.out.println(var1 - var2);
+        System.out.println(var1 * var2);
+        System.out.println(var1 / var2);
+        System.out.println(var1 % var2);
+        System.out.println(var1 > var2);
+        System.out.println(var1 >= var2);
+        System.out.println(var1 < var2);
+        System.out.println(var1 <= var2);
+        //System.out.println(var1 <> var2);
+        System.out.println(var1 != var2);
+        System.out.println(var1 << var2);
+        System.out.println(var1 >>> var2);
+        //System.out.println(var1 && var2);
+        //System.out.println(var1 || var2);
+        System.out.println(var1 & var2);
+        System.out.println(var1 | var2);
+    }
+}
